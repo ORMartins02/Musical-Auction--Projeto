f9c76dfe589e734f51c7e87264346073ca2b4666
amonfanticelli <amon_fanticelli@hotmail.com>
2022-09-01 10:27:47 -0300

    feat:conflitos-corrigidos-develop

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,6 +1,3 @@
 /// <reference types="react-scripts" />
-<<<<<<< HEAD
+
 declare module "*.svg";
-=======
-declare module "*.svg"
->>>>>>> 8c545b661cd4f0c5feb2176e60a5bcce35b462f0
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,7 @@
 import { Routes, Route } from "react-router-dom";
-<<<<<<< HEAD
+
 import { Dashboard } from "../pages/Dashboard/index";
 import Login from "../pages/Login/index";
-=======
-import { Dashboard } from "../pages/Dashboard";
->>>>>>> 8c545b661cd4f0c5feb2176e60a5bcce35b462f0
 
 export const MyRoutes = () => {
   return (
